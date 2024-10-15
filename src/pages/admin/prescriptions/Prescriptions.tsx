3b0de089da92a0f0cc8d6ab65ee553362a1cd75f
anhdev1104 <anhbhpd08263@fpt.edu.vn>
2024-10-16 00:36:08 +0700

    refactor code

--- src/pages/admin/prescriptions/Prescriptions.tsx
+++ src/pages/admin/prescriptions/Prescriptions.tsx
@@ -165,14 +165,14 @@ const Prescriptions = () => {
                             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                             onClick={() => handleToggle(item.id)}
                           >
-                            <i className="fa-solid fa-pen-to-square mr-2"></i> Sửa
+                            Sửa
                           </Link>
                           <Link
                             to={'#'}
                             className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                             onClick={() => handleToggle(item.id)}
                           >
-                            <i className="fa fa-trash-alt mr-2"></i> Xóa bỏ
+                            Xóa bỏ
                           </Link>
                         </div>
                       )}
