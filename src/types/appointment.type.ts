13e0e5e88c5bd2b9c3d63e33315efd560301dbef
anhdev1104 <anhbhpd08263@fpt.edu.vn>
2024-11-15 22:47:25 +0700

    #30 - handle feature appointments managenment

--- src/types/appointment.type.ts
+++ src/types/appointment.type.ts
@@ -4,6 +4,7 @@ export interface IAppointment {
   phone_number: string;
   address: string;
   gender: string;
+  description?: string;
   dob: string;
   appointment_date: string | null;
   specialty_id: string;
