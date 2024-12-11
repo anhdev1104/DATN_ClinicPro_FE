a5465a9d1a66ba86887997040f328f330e805e12
anhdev1104 <anhbhpd08263@fpt.edu.vn>
2024-12-11 10:43:55 +0700

    #27 - refactor

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -37,3 +37,4 @@ export { default as PersonRemoveIcon } from '@mui/icons-material/PersonRemove';
 export { default as EditNoteIcon } from '@mui/icons-material/EditNote';
 export { default as DeleteIcon } from '@mui/icons-material/Delete';
 export { default as FileDownloadIcon } from '@mui/icons-material/FileDownload';
+export { default as MedicalServicesIcon } from '@mui/icons-material/MedicalServices';
